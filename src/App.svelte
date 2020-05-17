774c90e052619b8d2b418f411bed28757109b449
Max <max@mproske.com>
2020-05-17 14:44:28 -0700

    Baby's first event listener

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,15 @@
 <script>
-  export let name
+  let clicks = 0
+
+  const handleClick = () => clicks++
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
+  <button on:click={handleClick}>
+    {clicks} {clicks === 1 ? 'Click' : 'Clicks'}
+  </button>
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
